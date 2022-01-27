6c368b0a297254584ce3b41b3a0c8aea4bc30256
BertyBasset <reverendrythym@googlemail.com>
2022-01-27 18:30:57 +0000

    Update Sniffer.h

--- Sniffer.h
+++ Sniffer.h
@@ -14,11 +14,6 @@ struct SniffSummary {
 typedef void(*callbackPtr)(SniffSummary);
 callbackPtr cb_sniffer;
 
-
-
-
-
-
 #define TYPE_MANAGEMENT       0x00
 #define TYPE_CONTROL          0x01
 #define TYPE_DATA             0x02
@@ -167,6 +162,16 @@ void channelHop()
 #define ENABLE  1
 
 
+void enableSniffing() {
+  wifi_promiscuous_enable(ENABLE);
+  os_timer_arm(&channelHop_timer, CHANNEL_HOP_INTERVAL_MS, 1);
+}
+
+void disableSniffing() {
+  wifi_promiscuous_enable(DISABLE);
+  os_timer_disarm(&channelHop_timer); 
+}
+
 
 void initSniffer(callbackPtr ptr) {
   cb_sniffer = ptr;
@@ -180,10 +185,14 @@ void initSniffer(callbackPtr ptr) {
   delay(10);
   wifi_set_promiscuous_rx_cb(sniffer_callback);
   delay(10);
-  wifi_promiscuous_enable(ENABLE);
+ // wifi_promiscuous_enable(ENABLE);
 
   // setup the channel hoping callback timer
   os_timer_disarm(&channelHop_timer);
   os_timer_setfn(&channelHop_timer, (os_timer_func_t *) channelHop, NULL);
-  os_timer_arm(&channelHop_timer, CHANNEL_HOP_INTERVAL_MS, 1);
+
+  enableSniffing();
+
+  //os_timer_arm(&channelHop_timer, CHANNEL_HOP_INTERVAL_MS, 1);
 }
+
